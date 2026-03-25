527e633192159e0870c5c0bf29129550387054dc
ronnymunzhedzi <ronewamunzhedzi85@gmail.com>
2026-03-25 14:31:36 +0200

    added tNode class and basic BST operations

--- tryBST.java
+++ tryBST.java
@@ -17,5 +17,29 @@ public class tryBST{
       root = null;
       size = 0;
     }
+    public void insert (int key) {
+      root =insertRec(root,key);
+      size++;
+    }
+    private tNode insertrec(tNode ,int key) {
+      if (node==null){
+        return new tNode (key);
+      }
+      if (key < node.key){
+        node.left = insertRec(node.left,key);
+      } else if (key> node.key){
+        node.right = insertRec( node.right,key);
+        return node;
+      }
+      public boolean delete(int key) {
+        if (search(key)){
+        root = deleteRec(root,key):
+          size--;
+        return true;
+      }
+      return false;
+    }
+    private tNode
+        
     
       
